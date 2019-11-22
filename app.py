08e817772df0150d9337a5c1c04e5fb6cc37e5c9
giladkah <gilad.kahala@gmail.com>
2019-11-22 13:30:15 -0500

    [migrations][pre commit] (#1)

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from sqlalchemy import MetaData
+import logging
+from config import LocalConfig, TestingConfig, StagingConfig, ProductionConfig
+
+
+DEVELOPMENT = 'development'
+TESTING = 'testing'
+STAGING = 'staging'
+PRODUCTION = 'production'
+
+
+app = Flask(__name__, static_folder="./client/dist", template_folder="./client/build")
+
+config_map = {
+    DEVELOPMENT: LocalConfig,
+    TESTING: TestingConfig,
+    STAGING: StagingConfig,
+    PRODUCTION: ProductionConfig,
+}
+config = config_map[app.env]
+app.config.from_object(config)
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+def _get_logger(log_level):
+    logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)  # type: ignore 
+migrate = Migrate(app, db)
+ma = Marshmallow(app)
+logger = _get_logger(config.LOG_LEVEL)
+
+
+
