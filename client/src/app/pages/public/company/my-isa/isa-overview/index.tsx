d9c14ea466d7f3de047d5fedee68e2d284af2082
islambek-toichuev <9sennheiser9@gmail.com>
2020-08-02 16:01:55 +0300

    list of isas and single isa

--- client/src/app/pages/public/company/my-isa/isa-overview/index.tsx
+++ client/src/app/pages/public/company/my-isa/isa-overview/index.tsx
@@ -53,65 +53,67 @@ export default function IsaOverview(props: IsaOverviewProps) {
       <PageHeader user={props.currentUser} />
       {loading ? (
         <Loader />
-      ) : isa && (
-        <PageContent>
-          <h1 className="page-title">Dina Castro</h1>
-          <section className="form">
-            <Tabs tabs={['overview', 'isa offer']} onChange={tab => set_active_tab(tab)} activeTab={active_tab} />
-            {active_tab === 'overview' ? (
-              <div className="overview">
-                <div>
-                  <label>Total Paid</label>
-                  <p>
-                    5,000 USD
-                    <span>of 25,000 USD</span>
-                  </p>
+      ) : (
+        isa && (
+          <PageContent>
+            <h1 className="page-title">Dina Castro</h1>
+            <section className="form">
+              <Tabs tabs={['overview', 'isa offer']} onChange={tab => set_active_tab(tab)} activeTab={active_tab} />
+              {active_tab === 'overview' ? (
+                <div className="overview">
+                  <div>
+                    <label>Total Paid</label>
+                    <p>
+                      5,000 USD
+                      <span>of 25,000 USD</span>
+                    </p>
+                  </div>
+                  <div>
+                    <label>Total Paid</label>
+                    <p>
+                      1,250 USD
+                      <span>August 8th 2020</span>
+                    </p>
+                  </div>
+                  <div>
+                    <label>Total Paid</label>
+                    <p>95K / YEAR</p>
+                  </div>
+                  <div>
+                    <label>Total Paid</label>
+                    <p>
+                      125K / YEAR
+                      <span>Last verified: August 8th 2020</span>
+                    </p>
+                  </div>
                 </div>
-                <div>
-                  <label>Total Paid</label>
-                  <p>
-                    1,250 USD
-                    <span>August 8th 2020</span>
-                  </p>
-                </div>
-                <div>
-                  <label>Total Paid</label>
-                  <p>95K / YEAR</p>
-                </div>
-                <div>
-                  <label>Total Paid</label>
-                  <p>
-                    125K / YEAR
-                    <span>Last verified: August 8th 2020</span>
-                  </p>
-                </div>
-              </div>
-            ) : (
-              <>
-                <Status selected="paying" />
-                <div className="fields">
-                  <Field title="company">{makeName(props.currentUser)}</Field>
-                  <Field title="Client">{makeName(isa.student)}</Field>
-                  <Field className="full" title="Current Income">
-                    ${isa.current_income}K / YEAR
-                  </Field>
-                  <Field className="full" title="time to be paid">
-                    {isa.time_to_be_paid || '-'} weeks
-                  </Field>
-                  <Field className="full" title="Percentage to be paid">
-                    {isa.percentage || '-'}%
-                  </Field>
-                  <Field className="full" title="Description">
-                    {isa.description || '-'}
-                  </Field>
-                  <Field className="full" title="Cancellation PEriod">
-                    {isa.cap || '-'} weeks
-                  </Field>
-                </div>
-              </>
-            )}
-          </section>
-        </PageContent>
+              ) : (
+                <>
+                  <Status selected="paying" />
+                  <div className="fields">
+                    <Field title="company">{makeName(props.currentUser)}</Field>
+                    <Field title="Client">{makeName(isa.student)}</Field>
+                    <Field className="full" title="Current Income">
+                      ${isa.current_income}K / YEAR
+                    </Field>
+                    <Field className="full" title="time to be paid">
+                      {isa.time_to_be_paid || '-'} weeks
+                    </Field>
+                    <Field className="full" title="Percentage to be paid">
+                      {isa.percentage || '-'}%
+                    </Field>
+                    <Field className="full" title="Description">
+                      {isa.description || '-'}
+                    </Field>
+                    <Field className="full" title="Cancellation PEriod">
+                      {isa.cap || '-'} weeks
+                    </Field>
+                  </div>
+                </>
+              )}
+            </section>
+          </PageContent>
+        )
       )}
     </article>
   )
