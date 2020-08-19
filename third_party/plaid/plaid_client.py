77805d46799f5c564d352631fdc77661099383da
gilad kahala <gilad@askpillar.com>
2020-08-19 17:56:54 +0300

    [more changes]

--- third_party/plaid/plaid_client.py
+++ third_party/plaid/plaid_client.py
@@ -1,3 +1,4 @@
+import time
 from common.exceptions import ValidationError
 from app import config, logger
 from plaid import Client
@@ -29,3 +30,14 @@ def get_access_token(public_token: str) -> str:
 def get_plaid_accounts(access_token: str) -> Dict[Any, Any]:
     accounts = client.Accounts.get(access_token)  # type: Dict[Any, Any]
     return accounts
+
+
+def get_link_token() -> Any:
+    plaid_configs = {
+        "user": {"client_user_id": str(time.time()),},
+        "products": ["auth", "transactions"],
+        "client_name": "Commit Muse",
+        "country_codes": ["US"],
+        "language": "en",
+    }
+    return client.LinkToken.create(plaid_configs)
