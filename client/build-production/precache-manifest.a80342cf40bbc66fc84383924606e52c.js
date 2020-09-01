ada4b294354ea37511fae60976e9b5a99a2dfcc4
islambek-toichuev <9sennheiser9@gmail.com>
2020-09-01 16:50:09 +0300

    build

---
+++ client/build-production/precache-manifest.a80342cf40bbc66fc84383924606e52c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b949cc73d4f12a6477564ae7655b8760",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "1aec46b94bfa53e4ad5c",
+    "url": "/web/static/css/main.377ee42e.chunk.css"
+  },
+  {
+    "revision": "37dbc984d39706c2aba6",
+    "url": "/web/static/js/2.ccaf70a0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/web/static/js/2.ccaf70a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aec46b94bfa53e4ad5c",
+    "url": "/web/static/js/main.533c2a45.chunk.js"
+  },
+  {
+    "revision": "0fbc79aab8b8a7373800",
+    "url": "/web/static/js/runtime-main.f5548c72.js"
+  },
+  {
+    "revision": "45d28c39432d165faa2aa7d78513d584",
+    "url": "/web/static/media/account.45d28c39.svg"
+  },
+  {
+    "revision": "33964afc3954c93e7bbf4e9f068f32a6",
+    "url": "/web/static/media/arrow-right.33964afc.svg"
+  },
+  {
+    "revision": "5feb3c9488fbf799114e1b706f5cbf0a",
+    "url": "/web/static/media/file-contract.5feb3c94.svg"
+  }
+]);
