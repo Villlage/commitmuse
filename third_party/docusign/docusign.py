f4250abd54caf5cfee7903434b4d2c865b1e0f24
giladkah <gilad.kahala@gmail.com>
2020-09-07 16:24:10 +0300

    Fix authorization (#76)
    * [fix authorization]
    
    * [more]
    
    Co-authored-by: gilad kahala <gilad@askpillar.com>

--- third_party/docusign/docusign.py
+++ third_party/docusign/docusign.py
@@ -44,7 +44,7 @@ class DSClient:
                 consent_scopes = "signature%20impersonation"
                 redirect_uri = config.WEB_APP_SERVER + url_for("ds_callback")
                 consent_url = (
-                    f"{config.AUTHORIZATION_SERVER}/oauth/auth?response_type=code&"
+                    f"https://{config.AUTHORIZATION_SERVER}/oauth/auth?response_type=code&"
                     f"scope={consent_scopes}&client_id={config.DOCUSIGN_CLIENT_ID}&redirect_uri={redirect_uri}"
                 )
                 return redirect(consent_url)
@@ -101,7 +101,7 @@ class DSClient:
         """Make request to the API to get the user information"""
         # Determine user, account_id, base_url by calling OAuth::getUserInfo
         # See https://developers.docusign.com/esign-rest-api/guides/authentication/user-info-endpoints
-        url = config.AUTHORIZATION_SERVER + "/oauth/userinfo"
+        url = "https://" + config.AUTHORIZATION_SERVER + "/oauth/userinfo"
         auth = {"Authorization": "Bearer " + access_token}
         response = requests.get(url, headers=auth).json()
 
