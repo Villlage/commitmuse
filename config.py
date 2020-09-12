eebe57c0e27870f4a34fd33d0e44d1b92de8bea5
islambek-toichuev <9sennheiser9@gmail.com>
2020-09-12 18:01:12 +0300

    fix cors issue

--- config.py
+++ config.py
@@ -49,7 +49,8 @@ class Config(object):
 class LocalConfig(Config):
     DEBUG = True
     LOG_LEVEL = "DEBUG"
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/village"
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
 
     WEB_APP_DOMAIN = "http://localhost:5000/web"
     WEB_APP_SERVER = "http://localhost:5000"
