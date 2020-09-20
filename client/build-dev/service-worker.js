7f1c3fffdf414dc644279e638ee6d6eac371489e
islambek-toichuev <9sennheiser9@gmail.com>
2020-09-20 14:46:20 +0300

    build

--- client/build-dev/service-worker.js
+++ client/build-dev/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web/precache-manifest.2993cb95019540afeb83df3437035207.js"
+  "/web/precache-manifest.fea93c626c988478c2123838bcebc9b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
